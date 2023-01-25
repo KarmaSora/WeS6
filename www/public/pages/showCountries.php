a3d19190341fa86df4c52c9356d8b6eb624341b1
KarmaSora <karammtr@gmail.com>
2023-01-25 13:30:41 +0100

    code copy from theory document

---
+++ www/public/pages/showCountries.php
@@ -0,0 +1,20 @@
+<?php    
+    include_once('inc/db.inc.php');
+    /* Bygger upp sql frågan */
+    $sqlkod = "SELECT Name, Population FROM country WHERE Name LIKE 'S%' AND Continent = 'Europe'
+                  ORDER BY Name";    
+    
+    /* Kör frågan mot databasen world och tabellen country */
+    $stmt = $db->prepare($sqlkod);    
+    $stmt->execute();
+ 
+    /* Anger teckenkodningen för webbläsaren */
+    header('Content-Type: text/html; charset=utf-8');
+    
+    /* skriver ut resultatet på webbsidan. */
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        echo "<strong>Land: </strong>".$row['Name'];
+        echo " <strong>Antal invånare: </strong>".$row['Population'];
+        echo "<br /><hr />";
+    }
+?>
